c8d236c38f4fa958483b0a552b078116e6b095de
Tanjila Shamima <tanjila.cse.diu@gmail.com>
2025-05-24 20:09:20 +0600

    add block in chain

--- Coding A Simple Blockchain/index.js
+++ Coding A Simple Blockchain/index.js
@@ -16,16 +16,40 @@ class Block {
   }
 }
 
+class Blockchain {
+  constructor() {
+    this.chain = [];
+  }
+
+  addBlock(newBlock){
+    // if (this.chain.length > 0) {
+    //   newBlock.prevHash = this.chain[this.chain.length - 1].hash;
+    // }
+    // newBlock.hash = newBlock.calculateHash();
+    this.chain.push(newBlock);
+  }
+}
+
 const block1 = new Block("01/01/2023", { amount: 4 }, "abcdef12345");
-const block2 = new Block("02/01/2023", { amount: 10 }, block1.hash);
-const block3 = new Block("03/01/2023", { amount: 20 }, block2.hash);
-const blockchain = [block1, block2, block3];
-console.log("Blockchain:", blockchain);
-blockchain.forEach((block, index) => {
-  console.log(`Block ${index + 1}:`);
-  console.log(`Timestamp: ${block.timestamp}`);
-  console.log(`Data: ${JSON.stringify(block.data)}`);
-  console.log(`Previous Hash: ${block.prevHash}`);
-  console.log(`Hash: ${block.hash}`);
-  console.log("-------------------------");
-});
+// const block2 = new Block("02/01/2023", { amount: 10 }, block1.hash);
+// const block3 = new Block("03/01/2023", { amount: 20 }, block2.hash);
+
+// const blockchain = [block1, block2, block3];
+// console.log("Blockchain:", blockchain);
+// blockchain.forEach((block, index) => {
+//   console.log(`Block ${index + 1}:`);
+//   console.log(`Timestamp: ${block.timestamp}`);
+//   console.log(`Data: ${JSON.stringify(block.data)}`);
+//   console.log(`Previous Hash: ${block.prevHash}`);
+//   console.log(`Hash: ${block.hash}`);
+//   console.log("-------------------------");
+// });
+
+
+
+// Create a simple blockchain with the blocks
+const jossCoin = new Blockchain();
+jossCoin.addBlock(block1);
+
+console.log("JossCoin Blockchain:", jossCoin);
+
